4f60697377d428b11586f9471ce7370a06bbb3c5
KimSehyeoun <rla6170@naver.com>
2024-03-11 14:09:46 +0900

    add example for dir

---
+++ Vino-PWA/src/constants/example.ts
@@ -0,0 +1,4 @@
+const EXAMPLE = 3.14;
+
+
+export default EXAMPLE;
---
+++ Vino-PWA/src/hooks/example.ts
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+const hook = () => {
+    const [num, setNum] = useState();
+    return num;
+}
