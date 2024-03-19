1ae7b81cdbf7dee96a47625b72062e4e9c0b5645
KimSehyeoun <rla6170@naver.com>
2024-03-19 13:21:01 +0900

    styled component reset

--- Vino-PWA/vite.config.js
+++ Vino-PWA/vite.config.js
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-  svgr({
-    svgrOptions : {
 
-    }
-  })]
+export default defineConfig({
+  plugins: [svgr(), react()],
+  resolve : {
+    alias : [
+      { find : '@', replacement : "/src"},
+      { find : '@assets', replacement : "/src/assets"},
+      { find : '@components', replacement : 'src/components'},
+      { find : '@styles', replacement : '/src/styles'}
+    ]
+  }
 })
